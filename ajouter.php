0021c6612db692568a9f28ecf5c9994b3aa5a698
salma <salmatergui7@gmail.com>
2024-12-20 19:02:00 +0100

    modification crud (delte)

--- ajouter.php
+++ ajouter.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit'])) {
       echo "Statistic player data inserted successfully! ID: " . $id_statistic_player . "<br>";
   } else {
       echo "Error: " . $stmt->error . "<br>";
-      exit; 
+     
   }
   $stmt->close();
 
@@ -36,7 +36,7 @@ if (isset($_POST['submit'])) {
       echo "Nationnalite data inserted successfully! ID: " . $id_nationnalite . "<br>";
   } else {
       echo "Error: " . $stmt->error . "<br>";
-      exit; 
+      
   }
   $stmt->close();
 
